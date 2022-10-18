8687fd90a156e87e38c0df134edaf7768ea9dc76
DandinPower <tomhot246@gmail.com>
2022-10-18 13:16:03 +0800

    adjust layout

--- homework1/fileIO/fileIO.c
+++ homework1/fileIO/fileIO.c
@@ -9,66 +9,59 @@
 #include <linux/mm.h> 
 #include <asm/uaccess.h> 
  
+#define BUFFER_SIZE 300
 MODULE_DESCRIPTION("read file in and write it out in inverse"); 
 MODULE_AUTHOR("Yong-Cheng Liao<tomhot246@gmail.com>"); 
 MODULE_ALIAS("read file & write file module");
 MODULE_LICENSE("GPL"); 
 
-int read_file(void){
-    struct file *io;
-    void *buf;
-    size_t count;
-    loff_t pos = 0;
-    ssize_t rx;
-    char tmp_char[100];
-    memset(tmp_char, 0, sizeof(tmp_char));
+void reverse(char *str){
+    int i,j;
+    char temp;
+    j = strlen(str) - 1;
+    str[j + 1] = '\n';
+    for (i=0; i<j; i++){
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        j--;
+    }
+}
 
-    io = filp_open("input.txt", O_GREAT | O_RDONLY, 0);
+static int fileIO_init(void) 
+{ 
+    struct file *io;
+    char tmp_char[BUFFER_SIZE];
+    void* buf;
+    ssize_t rx;
+    ssize_t tx;
+    size_t count;
+    size_t writeCount;
+    loff_t pos = 0;
+    memset(tmp_char, 0, sizeof(tmp_char));
+    io = filp_open("input.txt", O_RDONLY, 0);
     if (IS_ERR(io)){
         printk("open file error/n");
         return -1;
     }
     count = sizeof(tmp_char);
-    buf = (void *)(&tmp_char);
-    rx = kernel_read(io, buf, count, &pos);
-    printk("%s, read result: tmp_char=%s\n", __func__, tmp_char);
+    buf = (void*)tmp_char;
+    rx = kernel_read(io, tmp_char, count, &pos);
+    printk("read result= %s\n", tmp_char);
+    reverse(tmp_char);
+    printk("reverse result= %s\n", tmp_char);
     filp_close(io, NULL);
-    return 0;
-}
-
-int write_file(void){
-    struct file *io;
-    void *buf;
-    size_t count;
-    loff_t pos = 0;
-    ssize_t tx;
-    char *tmp_char = "hello world";
-    //memset(tmp_char, 0, sizeof(tmp_char));
-
-    io = filp_open("output.txt", O_GREAT | O_RDWR, 0664);
+    pos = 0;
+    io = filp_open("output.txt", O_CREAT | O_RDWR, 0664);
     if (IS_ERR(io)){
         printk("open file error/n");
         return -1;
     }
-    count = strlen(tmp_char) + 1;
-    buf = (void *)(&tmp_char);
-    tx = kernel_write(io, buf, count, &pos);
-    //printk("%s, read result: tmp_char=%s\n", __func__, tmp_char);
+    buf = (void *)tmp_char;
+    writeCount = strlen(tmp_char);
+    tx = kernel_write(io, buf, writeCount, &pos);
     filp_close(io, NULL);
     return 0;
-}
-
-static int fileIO_init(void) 
-{ 
-    /*
-    io = filp_open("output.txt", O_RDWR, 0664);
-    if (IS_ERR(io)){
-        printk("create file error/n");
-        return -1;
-    }
-    */
-    //int status = read_file();
-    return write_file();
 } 
  
 static void fileIO_exit(void) 
